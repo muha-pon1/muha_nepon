b9cf298c5c87b50573915a958ce1920e9029f719
peeper4 <mishatronic7045@gmail.com>
2025-10-08 16:43:42 +0300

    f

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// 2.4.5
+
+// let str = " aaa bbb ccc"
+// let newstr = str
+
+// for (let i = 0; i < str.length; i++) {
+//     const element = str[i];
+//     if (element == " " || !str[i-1]) {    
+//         newstr = str.replace(str[i+1], str[i+1].toUpperCase())
+//         str = newstr
+//         console.log(newstr)
+//     }
+// }
+// console.log(newstr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
