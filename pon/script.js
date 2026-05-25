d39551a3a6f202765aad38118bd790319a9fdc6e
muha <mishatronic7045@gmail.com>
2026-05-25 16:11:15 +0300

    G

--- pon/script.js
+++ pon/script.js
@@ -1,1 +1,18 @@
+// 3.1.1
+
+const num = 18763
+const numStr = num.toString()
+
+
+
+for (let i = 0; i < numStr.length - 1; i++) {
+    const element = numStr[i];
+      
+    if (element < numStr[i+1] ) {
+       console.log(numStr);
+ 
+    
+    }
+}
+
 
