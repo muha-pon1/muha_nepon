b4cdef4daab9d91341e53e7156477afa820270e9
muha <mishatronic7045@gmail.com>
2026-06-01 17:11:40 +0300

    h

--- pon/script.js
+++ pon/script.js
@@ -1,1 +1,15 @@
+const log = document.getElementById('log')
+const pass = document.getElementById('pass')
+const reg = document.getElementById('reg')
+
+function regid() {
+    
+    if (log.value.indexof != "@" && log.value.indexof != ".") {
+        
+        console.log("error");
+
+    } 
+
+}
+
 
