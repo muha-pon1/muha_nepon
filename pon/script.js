a21fabf02743ad86675f1873261c770ee2150c61
muha <mishatronic7045@gmail.com>
2026-06-08 17:15:44 +0300

    Update script.js

--- pon/script.js
+++ pon/script.js
@@ -1,15 +1,16 @@
 const log = 'sdfds@sdda.da';
+const pass = 'Adgsdgsqw2423'
 
-reg(log)
+reg(log, pass);
 
-function reg(log) {
+
+function reg(log, pass) {
     let log1;
-
-     
+    let pass1;
 
   if (log !== 0) {
-      const atSignIndex = log.indexOf('@')
-      const pointIndex = log.indexOf('.')
+      const atSignIndex = log.indexOf('@');
+      const pointIndex = log.indexOf('.');
       
       if (
         atSignIndex !== -1 &&
@@ -18,15 +19,29 @@ function reg(log) {
         && log.slice(atSignIndex + 1, pointIndex).length > 0
         && log.slice(pointIndex + 1, log.length).length > 0    
     ){
-        log1 = true
-     } else log1 = false
-      
+        log1 = true;
+     } else log1 = false;
+}
 
 
-  }
-console.log(log1);
 
+  const hasNum = /[A-Z]/.test(pass)
+  const upperCase = /\d/.test(pass)
+  const isLengthCorrect = pass.length >= 8? true : false;
+  
+  if (hasNum && isLengthCorrect && upperCase) {
+    pass1 = true
+  } else pass1 = false
+
+
+
+    console.log(log1);
+    console.log(pass1);
+
+  
+  return log1 && pass1 ? true : false;
 
 }
+
 
 
